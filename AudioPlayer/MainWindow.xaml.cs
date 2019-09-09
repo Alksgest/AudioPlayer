6804072e4725fef4027616d35b91dc457cd6bc94
Vladyslav <alksgest@gmail.com>
2019-09-09 22:36:57 +0300

    added initial wrapper for naudio

---
+++ AudioPlayer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using AudioWorker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AudioPlayer
+{
+    public partial class MainWindow : Window
+    {
+        IAudioPlayer _player;
+
+        private const string _sampleAudio = @"C:\Personal Data\01 - Black Star.mp3";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _player = AudioPlayerFactory.GetAudioPlayer();
+            _player.InitAudio(_sampleAudio);
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Play();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _player.Stop();
+        }
+    }
+}
---
+++ AudioWorker/AudioPlayer.cs
@@ -0,0 +1,88 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioWorker
+{
+    public interface IAudioPlayer
+    {
+        void Play();
+        void Stop();
+        void Pause();
+
+        void InitAudio(string path);
+
+        TimeSpan? GetCurrentPosition { get; }
+        TimeSpan? GetDuration { get; }
+        //return sometype
+        /*AudioData*/
+        int GetAudioData { get; }
+
+        void ChangeVolume(float value);
+    }
+
+    public static class AudioPlayerFactory
+    {
+        public static IAudioPlayer GetAudioPlayer()
+        {
+            return new AudioPlayer();
+        }
+    }
+
+    public class AudioPlayer : IAudioPlayer
+    {
+        private WaveOutEvent _waveOutEvent;
+        private AudioFileReader _fileReader;
+
+        public TimeSpan? GetCurrentPosition => _fileReader?.CurrentTime;
+
+        public TimeSpan? GetDuration => _fileReader.TotalTime;
+
+        public int GetAudioData => 0;
+
+        public AudioPlayer()
+        {
+            _waveOutEvent = new WaveOutEvent();
+
+
+
+        }
+
+        public void InitAudio(string path)
+        {
+            _fileReader = new AudioFileReader(path);
+            _waveOutEvent.Init(_fileReader);
+        }
+
+        /// <summary>
+        /// Value should be between 0.0 and 1.0
+        /// </summary>
+        public void ChangeVolume(float value)
+        {
+            if (value < 0 || value > 1)
+                return;
+            _waveOutEvent.Volume = value;
+        }
+
+        public void Pause()
+        {
+            if (_fileReader != null)
+                _waveOutEvent.Pause();
+        }
+
+        public void Play()
+        {
+            if (_fileReader != null)
+                _waveOutEvent.Play();
+        }
+
+        public void Stop()
+        {
+            if (_fileReader != null)
+                _waveOutEvent.Stop();
+        }
+    }
+}
